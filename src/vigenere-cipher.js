e5dfd1d5993faaf407af2302e8aab4b19a29f556
Vanyha010 <vanyha010@icloud.com>
2023-04-13 23:58:34 +0300

    add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  insertAtIndex(str, substring, index) {
+    return str.slice(0, index) + substring + str.slice(index);
+  }
+
+  commonCase(string, key) {
+    let str = string.toUpperCase();
+    let newKey = key.toUpperCase();
+    newKey = newKey.padEnd(str.length, `${newKey}`);
+    return [str, newKey]
+  }
+  encrypt(string, key) {
+    if (typeof string === 'undefined' || typeof key === 'undefined') throw Error('Incorrect arguments!');
+    let [str, newKey] = this.commonCase(string, key);
+    let alphabet = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').repeat(5);
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      if (alphabet.includes(str[i])) {
+        let stringIndex = alphabet.indexOf(str[i]);
+        let keyIndex = alphabet.indexOf(newKey[i]);
+        result = `${result}${alphabet[stringIndex + keyIndex]}`
+      } else {
+        newKey = this.insertAtIndex(newKey, str[i], i);
+        result = `${result}${str[i]}`;
+      }
+    }
+    if (this.direct) {
+      result
+    } else {
+      result = result.split('').reverse().join('')
+    }
+    return result;
+  }
+  decrypt(string, key) {
+    if (typeof string === 'undefined' || typeof key === 'undefined') throw Error('Incorrect arguments!');
+    let [str, newKey] = this.commonCase(string, key);
+    let alphabet = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').repeat(5);
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      if (alphabet.includes(str[i])) {
+        let stringIndex = alphabet.lastIndexOf(str[i]);
+        let keyIndex = alphabet.indexOf(newKey[i]);
+        result = `${result}${alphabet[stringIndex - keyIndex]}`
+      } else {
+        newKey = this.insertAtIndex(newKey, str[i], i);
+        result = `${result}${str[i]}`;
+      }
+    }
+    if (this.direct) {
+      result
+    } else {
+      result = result.split('').reverse().join('')
+    }
+    return result;
   }
 }
 
