5cbe1929f8be64601e3857a9f193e44abb6738fa
Vanyha010 <vanyha010@icloud.com>
2023-04-14 18:51:13 +0300

    add dns-stats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const obj = {};
+  domains.forEach(item => {
+    const arr = item.split('.').reverse();
+    arr.forEach((elem, index) => {
+      const key = '.'.concat(arr.slice(0, index + 1).join('.'));
+      if (obj.hasOwnProperty(key)) {
+        obj[key]++
+      } else {
+        obj[key] = 1;
+      }
+    });
+  });
+  return obj;
 }
 
 module.exports = {
